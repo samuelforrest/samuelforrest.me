2727d0c72a9956c140589bff6cd93a9ed07560a5
Samuel Forrest <sam@samuelforrest.me>
2025-09-20 17:12:01 +0100

    content changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -27,7 +27,6 @@ const socialLinks = [
   { href: "https://github.com/samuelforrest", icon: Github, label: "GitHub" },
   { href: "https://linkedin.com/in/samueljforrest", icon: Linkedin, label: "LinkedIn" },
   { href: "https://instagram.com/samueljforrest", icon: Instagram, label: "Instagram" },
-  { href: "https://instagram.com/samueljforrest", icon: Twitter, label: "Instagram" },
   { href: "mailto:sam@samuelforrest.me", icon: Mail, label: "Email" },
 ];
 
@@ -73,8 +72,8 @@ function ModeToggle() {
 
 export default function Navbar() {
   return (
-    <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14 safe-area-inset-bottom">
-      <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
+    <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-6 flex origin-bottom h-full max-h-14 safe-area-inset-bottom">
+      <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent dark:bg-background"></div>
       <Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] rounded-2xl max-w-fit"
         magnification={50}
         distance={120}
