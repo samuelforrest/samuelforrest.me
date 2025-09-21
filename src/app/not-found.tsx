547c40001dbc8974e1e869e1225a5f1355b3c399
Samuel Forrest <sam@samuelforrest.me>
2025-09-21 03:00:58 +0100

    Created 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-gray-800">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <p className="text-2xl mb-6">Oops! Page not found.</p>
+      <Link href="/" className="underline text-blue-500">
+          Go back home
+      </Link>
+    </div>
+  );
+}
