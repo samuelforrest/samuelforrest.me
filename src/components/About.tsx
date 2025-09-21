0e83f32a722032340d763d0484821bf6a50eaade
Samuel Forrest <sam@samuelforrest.me>
2025-09-21 15:45:53 +0100

    largest contentful paint element optimisation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
-
 export default function About() {
     return(
         <div>
             <h1 className="text-2xl font-bold pt-10">About</h1>
             
-            <p className="pt-4">I started coding when I was 10, adventuring into HTML, CSS and JavaScript. Fast forward to now I&apos;m 17, studying A Levels at St George&apos;s College, Weybridge, while building Tradelingo, an educational trading app for Gen Z. Along the way, I&apos;ve completed <Link href="#experience" className="underline underline-offset-4">work experience</Link> with British Airways, the AIIB, Amazon, HUDJO and Virgin Atlantic, while building my skills in teaching and communication as a tutor and developer. I have also participated in Hackathons, worked on <Link href="#projects" className="underline underline-offset-5">projects</Link> and enjoyed my <Link href="#extracurriculars" className="underline underline-offset-5">extracurriculars.</Link></p>
+            <p className="pt-4">I started coding when I was 10, adventuring into HTML, CSS and JavaScript. Fast forward to now I&apos;m 17, studying A Levels at St George&apos;s College, Weybridge, while building Tradelingo, an educational trading app for Gen Z. Along the way, I&apos;ve completed <a href="#experience" className="underline underline-offset-4">work experience</a> with British Airways, the AIIB, Amazon, HUDJO and Virgin Atlantic, while building my skills in teaching and communication as a tutor and developer. I have also participated in Hackathons, worked on <a href="#projects" className="underline underline-offset-5">projects</a> and enjoyed my <a href="#extracurriculars" className="underline underline-offset-5">extracurriculars.</a></p>
             <br></br>
             <p>I&apos;m looking for Degree Apprenticeships in Tech / Software Engineering, whilst applying to Uni.</p>
             
