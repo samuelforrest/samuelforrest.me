fbb196efa6967d6081fceceb92a192b8a6371352
Samuel Forrest <sam@samuelforrest.me>
2025-09-21 12:34:36 +0100

    changed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
             
             <div className="text-center pt-10 mb-25">
                 <p className="text-sm md:text-sm/relaxed lg:text-base/relaxed xl:text-sm/relaxed">&copy; Samuel Forrest - All rights reserved</p>
-                <p className="text-sm md:text-sm/relaxed lg:text-base/relaxed xl:text-sm/relaxed">Information is true, as to my best knowledge</p>
+                <p className="text-sm md:text-sm/relaxed lg:text-base/relaxed xl:text-sm/relaxed pt-3">📍 Crafted with passion in London, UK.</p>
             </div>
         </footer>
     )
