197882327ab026e57840a8bbcd17df4659250bce
Samuel Forrest <sam@samuelforrest.me>
2025-10-11 21:54:43 +0100

    more engaging 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,36 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-gray-800">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-2xl mb-6">Oops! Page not found.</p>
-      <Link href="/" className="underline text-blue-500">
-          Go back home
-      </Link>
-      <br></br>
-      <p className="text-sm mb-6 text-center pl-5 pr-5">If you were attempting to visit a Blog article, they are now available on&nbsp;
-        <a
+    <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen bg-white text-gray-800 p-8 gap-8">
+      <div className="flex-shrink-0">
+        <Image 
+          src="/404image.png" 
+          alt="404 Error Illustration"
+          width={500}
+          height={500}
+          className="w-full max-w-xs h-auto lg:max-w-md"
+        />
+      </div>
+      
+      <div className="flex flex-col items-center lg:items-start text-center lg:text-left">
+        <h1 className="text-4xl font-bold mb-4">404</h1>
+        <p className="text-md mb-6 max-w-md">
+          If you were attempting to visit a Blog article, they are now available on&nbsp;
+          <a
             href="https://samueljforrest.substack.com"
-            className="underline text-blue-500"
+            className="underline underline-offset-4 font-semibold"
             target="_blank"
             rel="noopener noreferrer"
-        >
+          >
             Substack
-        </a>
-      </p>
+          </a>
+        </p>
+        <Link href="/" className="underline underline-offset-4">
+          Go back home
+        </Link>
+      </div>
     </div>
   );
 }
