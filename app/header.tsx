f99c637618cab54b181f02569afb9010b6fef8c5
samuelforrest <sam@samuelforrest.me>
2026-01-12 19:05:32 +0000

    commit

--- app/header.tsx
+++ app/header.tsx
@@ -30,7 +30,7 @@ export function Header() {
             className="text-zinc-600 dark:text-zinc-500"
             delay={0.5}
           >
-            Student & Developer
+            Student, Developer & Tutor
           </TextEffect>
         </div>
       </div>
